7059bbcd940876ae9ae4b402bf2e05aeab8d3958
gitarthaqwerty@gmail.com <gitarthaqwerty@gmail.com>
2025-11-25 19:58:23 +0530

    new

---
+++ Local_practice_files/remove_duplicate.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = len(nums)
+        i = 0
+        while i < k:
+            j = 0
+            while j < k:
+                if i !=j and nums[i] == nums[j]:
+                    nums.pop(j)
+                    k -= 1
+                j += 1
+            i += 1
+        return k
